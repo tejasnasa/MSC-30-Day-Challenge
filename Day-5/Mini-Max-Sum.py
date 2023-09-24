224ad83bd4e58fd9ce90f7a21171540e03827635
tejasnasa <138145714+tejasnasa@users.noreply.github.com>
2023-09-24 11:54:05 +0530

    Create Mini-Max-Sum.py

---
+++ Day-5/Mini-Max-Sum.py
@@ -0,0 +1,16 @@
+import math
+import os
+import random
+import re
+import sys
+
+
+def miniMaxSum(arr):
+    arr.sort()
+    print(sum(arr) - arr[4], sum(arr) - arr[0])
+    
+if __name__ == '__main__':
+
+    arr = list(map(int, input().rstrip().split()))
+
+    miniMaxSum(arr)
