a6ebcdfcc55fe0ea7a5213b2cb9fc6db6dce9d92
tejasnasa <138145714+tejasnasa@users.noreply.github.com>
2023-09-25 16:18:00 +0530

    Create Number-Line-Jumps.py

---
+++ Day-6/Number-Line-Jumps.py
@@ -0,0 +1,33 @@
+import math
+import os
+import random
+import re
+import sys
+
+def kangaroo(x1, v1, x2, v2):
+    
+    for i in range (1,10000):
+        if (x1 + i * v1 == x2 + i * v2):
+            return ("YES")
+    
+            return ("NO")
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    x1 = int(first_multiple_input[0])
+
+    v1 = int(first_multiple_input[1])
+
+    x2 = int(first_multiple_input[2])
+
+    v2 = int(first_multiple_input[3])
+
+    result = kangaroo(x1, v1, x2, v2)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
