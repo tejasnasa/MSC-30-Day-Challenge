520f467dcce9ab23a5ca11b41cb290fbbccec85b
tejasnasa <138145714+tejasnasa@users.noreply.github.com>
2023-09-29 18:32:16 +0530

    Create Java-End-of-file.java

---
+++ Day-010/Java-End-of-file.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int x = 1;
+        while (sc.hasNextLine()) {
+            System.out.println(x + " " + sc.nextLine());
+            x++;
+        }
+    }
+}
