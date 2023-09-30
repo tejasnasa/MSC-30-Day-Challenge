46ab988438cff1f1cec72b273003de493c3105fc
tejasnasa <138145714+tejasnasa@users.noreply.github.com>
2023-09-30 19:47:03 +0530

    Create Viral-Advertising.java

---
+++ Day-011/Viral-Advertising.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+
+    public static int viralAdvertising(int n) {
+        int shared = 5;
+        int cumulative = 0;
+        int liked;
+        for (int i = 1; i < n + 1; i++) {
+            liked = shared / 2;
+            cumulative += liked;
+            shared = liked * 3;
+        }
+        
+        return cumulative;
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int result = Result.viralAdvertising(n);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
