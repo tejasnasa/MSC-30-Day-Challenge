9f45f1152ec4256e53a5c75f7c25ccbee339df11
tejasnasa <138145714+tejasnasa@users.noreply.github.com>
2023-10-01 20:57:09 +0530

    Create Save-the-Prisoner!.java

---
+++ Day-012/Save-the-Prisoner!.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    public static int saveThePrisoner(int n, int m, int s) {
+        int x = (s + m - 1) % n;
+        if(x == 0) {
+            x = n;
+        }
+        
+        return x;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+            int n = Integer.parseInt(firstMultipleInput[0]);
+
+            int m = Integer.parseInt(firstMultipleInput[1]);
+
+            int s = Integer.parseInt(firstMultipleInput[2]);
+
+            int result = Result.saveThePrisoner(n, m, s);
+
+            bufferedWriter.write(String.valueOf(result));
+            bufferedWriter.newLine();
+        }
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
