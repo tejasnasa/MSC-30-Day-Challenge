fee6c12d2a3902ed7e488ccc52e4b2c0cc9167e2
tejasnasa <138145714+tejasnasa@users.noreply.github.com>
2023-10-04 23:09:29 +0530

    Create CamelCase.java

---
+++ Day-015/CamelCase.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    public static int camelcase(String s) {
+        int n = 1;
+        for(int i = 1; i < s.length(); i++) {
+            if (Character.isUpperCase(s.charAt(i))) {
+                n++;
+            }
+        }
+        return n;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = bufferedReader.readLine();
+
+        int result = Result.camelcase(s);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
