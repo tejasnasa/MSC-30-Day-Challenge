b3b830957ca7200beda301874f238c7da8201474
tejasnasa <138145714+tejasnasa@users.noreply.github.com>
2023-10-08 21:42:04 +0530

    Create Pangrams.py

---
+++ Day-019/Pangrams.py
@@ -0,0 +1,24 @@
+import math
+import os
+import random
+import re
+import sys
+
+def pangrams(s):
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    s = s.lower()
+    for c in alphabet:
+        if c not in s:
+            return 'not pangram'
+    return 'pangram'
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = pangrams(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
