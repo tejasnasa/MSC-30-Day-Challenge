be6164c28f506d605120375e68d7fa7349f0b1a7
tejasnasa <138145714+tejasnasa@users.noreply.github.com>
2023-10-09 20:50:43 +0530

    Create Pointers-in-C.c

---
+++ Day-020/Pointers-in-C.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void update(int *a,int *b) {
+    int x = *a + *b;
+    int y = *a - *b;
+    
+    if (y < 0) {
+        y = -y;
+    }
+    
+    *a = x;
+    *b = y;    
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
