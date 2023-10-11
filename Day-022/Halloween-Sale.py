71171cdd5f456f0ede98d8707d3bd7921f2468a4
tejasnasa <138145714+tejasnasa@users.noreply.github.com>
2023-10-11 19:54:26 +0530

    Create Halloween-Sale.py

---
+++ Day-022/Halloween-Sale.py
@@ -0,0 +1,37 @@
+import math
+import os
+import random
+import re
+import sys
+
+def howManyGames(p, d, m, s):
+    ans = 0
+
+    while (s > 0):
+        ans += 1
+        s -= p
+        p = max(p - d, m)
+
+    if (s != 0):
+        ans -= 1
+
+    return ans
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    p = int(first_multiple_input[0])
+
+    d = int(first_multiple_input[1])
+
+    m = int(first_multiple_input[2])
+
+    s = int(first_multiple_input[3])
+
+    answer = howManyGames(p, d, m, s)
+
+    fptr.write(str(answer) + '\n')
+
+    fptr.close()
